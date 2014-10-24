c08be639b6b844ea59ea595cd71ce4b757ae3fc0
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-23 21:55:30 -0500

    trying to start the arduino test-example

--- rs232testduino/rs232testduino.ino
+++ rs232testduino/rs232testduino.ino
@@ -4,12 +4,22 @@ void setup() {
   Serial.begin(baudr);
 
   // Testing kfx::RS232::WriteByte
-
-  // Testing kfx::RS232::ReadByte
-
-  // Testing kfx::RS232::WriteBuf
+  byte a = Serial.ReadByte();
 
   // Testing kfx::RS232::ReadBuf
+  Serial.print("Caracte recibido: ");
+
+  // Testing kfx::RS232::ReadByte
+  Serial.print(a);
+  
+
+  // Testing kfx::RS232::WriteBuf
+  char A[80];
+  A = Serial.readBytes();
+  Serial.print("");
+
+
+
 }
 
 void loop() { }
